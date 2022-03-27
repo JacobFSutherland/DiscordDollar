6b8033053732170aba0b522f31c7e04a40ffa9ee
Fortnite-Burger <60914846+JacobFSutherland@users.noreply.github.com>
2022-03-26 19:42:41 -0700

    Add Economy participant

--- src/BlockData/EconomyParticipant.ts
+++ src/BlockData/EconomyParticipant.ts
@@ -2,8 +2,10 @@ import Stock from "./Goods/Stock";
 import Token from "./Goods/Token";
 import Option from "./Goods/Option";
 import TSMap from "./TSMap";
+import assert from "assert";
 
 export default class EconomyParticipant{
+    static addedIDs: string[] = [];
     discordID: string;
     tokens: TSMap<Token>;
     stocks: TSMap<Stock>;
@@ -14,6 +16,8 @@ export default class EconomyParticipant{
      * @param id The ID of the Discord user that is being initialized as an economy participant
      */
     constructor(id: string){
+        assert(!EconomyParticipant.addedIDs.includes(id), 'Duplicate Economy Participant');
+        EconomyParticipant.addedIDs.push(id);
         this.discordID = id; 
         this.tokens = {};
         this.stocks = {};
@@ -25,13 +29,20 @@ export default class EconomyParticipant{
      * @param token The token being added to the Discord user. 
      */
     addToken(token: Token){
-        if(this.tokens[token.name]) //Checking if the token exists within the user's known token balances
-            this.tokens[token.name].add(token);
-        
-    }
+        if(this.tokens[token.name]){ 
+            this.tokens[token.name].add(token); // if the token exists within the user's known token balances, the sums are totaled and updated accordingly.
+        }else{
+            this.tokens[token.name] = new Token(token.name, token.amount); // if the token does not exist within the user's known token balances, the token is cloned and set.
+        }//if
+    } //addToken
 
+    /**
+     * 
+     * @param token The token being subtracted from the Discord user. 
+     */
     removeToken(token: Token){
-
+        assert(this.tokens[token.name], `Token not found in user ${this.discordID}`);
+        this.tokens[token.name].remove(token); // if the token exists within the user's known token balances, the sums are totaled and updated accordingly.
     }
 
 
